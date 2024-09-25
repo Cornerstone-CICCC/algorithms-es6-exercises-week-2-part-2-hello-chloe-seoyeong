e7b519c9dbeb877f1e874be3f8acc84319c19076
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-25 11:23:41 -0700

    ex03 completed

--- ex03.js
+++ ex03.js
@@ -1,3 +1,7 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const randomNumbers = [5, 7, 10, 13, 16, 19, 22];
+
+console.log(randomNumbers.reverse());
+
