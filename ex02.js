48b4a7fdf0d44e1011b58cca41a6cca101156991
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-25 11:22:36 -0700

    ex02 completed

--- ex02.js
+++ ex02.js
@@ -1,3 +1,8 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+const randomNumbers = [5, 7, 10, 82, 100, 42, 3004];
+
+const greaterTen = randomNumbers.find((num) => num > 10);
+
+console.log(greaterTen);
