c6c9192e4cc51ab06bdef4f472e10c5866e78c6a
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-25 11:28:41 -0700

    ex05 completed

--- ex05.js
+++ ex05.js
@@ -1,3 +1,10 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const randomNumbers = [1, 63, 7, 33, 66, 185, 3];
+
+const checkEven = randomNumbers.some((num) => {
+  return num%2 === 0;
+})
+
+console.log(checkEven);
