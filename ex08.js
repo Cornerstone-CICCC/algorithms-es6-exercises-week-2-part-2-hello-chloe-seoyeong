56879c084d2f6e6726fda9037ea4f55daf5803c8
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-25 11:36:48 -0700

    ex08 completed

--- ex08.js
+++ ex08.js
@@ -1,3 +1,6 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const randomWords = ["I", "am", "really", "enjoying", "Vancouver", "life", "now. 😊"];
+
+console.log(randomWords.join(" "));
